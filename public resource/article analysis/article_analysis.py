9174e41306a6525b4e58d084a3de891d50724999
Jack Shi <shwzh1990@gmail.com>
2022-05-21 16:17:48 +1200

    update regular expression

--- public resource/article analysis/article_analysis.py
+++ public resource/article analysis/article_analysis.py
@@ -4,4 +4,24 @@ with open("CD_all.txt",'r',encoding='utf-8') as fd:
     data = fd.read()
 
 a = re.findall(r"Document CHNDLY...................", data, re.M)
-print(len(a))
+
+#print(len(a))
+#article = re.split(r"Document CHNDLY...................", data)
+#print(len(article))
+#import re
+##? ab? =>a, ab
+##+ ab+ => ab abbb abbbbbb
+##. 
+##\w any alphabet, character==
+##\W 
+##\s
+##\S
+##^  beginning of the string
+##[1-5]
+#str_sample = "ab,abbhjk, a5,a3,a6,abbcb, abbbjkbbbbbbbbb,a*,a#$%,a!$%$^$%#,a{,a ,"
+#result = re.findall("(a[1-5]),",str_sample)
+#print(result)
+str_sampe = "abdkljfadskl23fjfdkljklfjd45fgjreaklgj56dksjfgklfejr78fjeiejwklflk90fjakldsfjkdl"
+result_list = re.split(r"\d\d",str_sampe,re.ASCII)
+print(result_list)
+
