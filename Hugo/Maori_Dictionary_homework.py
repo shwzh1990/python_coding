1b450302bc26b8c599f73ef8a64661b76c89e5d9
Hugo <hugot@pmps.school.nz>
2022-07-12 11:30:46 +1200

    homework

---
+++ Hugo/Maori_Dictionary_homework.py
@@ -0,0 +1,4 @@
+print("Welcome to our Te Reo Maori Test!")
+Answer = input("please input which question type do you want:\n1.MultiSelection\n2.Word Answer\n3.Add Vocabulary\n")
+if Answer == "1":
+    input("please select different level:\n1.Easy\n2.Intermetiate\n3.Hard\n4.Random")
