3e0b7faad4ee9d5ff60823f261ebd29c4cf4c48e
Jack Shi <shwzh1990@gmail.com>
2022-07-30 14:33:58 +1200

    add review note

---
+++ Charles Xia/review.py
@@ -0,0 +1,44 @@
+#variables
+'''
+1. integer 45,2,3
+
+example:
+
+
+a = 45
+b = 23
+c = a + b
+print(c)
+
+result = input("XXXXX")
+
+result = input("please input some code")
+print(result)
+
+
+a = input("please input first number")
+b = input("please input second number")
+c = int(a) + int(b)
+print(c)
+
+2. String
+a = "wjfikhasfhskl" or b = "I am Charles"
+
+(1). string can be added
+
+example:
+a = "I am "
+b = "Jack" 
+c = a + b
+print(c)
+
+a = "I am Jack"
+my_list = a.split(" ")
+print(my_list)
+
+
+endwith
+'''
+a = "I am Charles"
+if a.endswith("Charles") == True:
+    print("Ok")
