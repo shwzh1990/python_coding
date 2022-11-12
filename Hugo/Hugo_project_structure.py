0138d554bcd850485b5e557adb58605385368d2d
shwzh1990 <shwzh1990@gmail.com>
2022-11-12 15:35:46 +1300

    update Hugo project structure

---
+++ Hugo/Hugo_project_structure.py
@@ -0,0 +1,19 @@
+{
+   "VT101":
+          {
+             "Ship_Mode":
+                        {
+                            "Success": 1
+                            "Failure": 2
+                        },
+            "OTAP_Mode":
+                       {
+
+                            "Success": 1
+                            "Failure": 2
+                       }
+
+          }
+
+
+}
